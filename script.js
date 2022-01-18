c919baa85f52f01bb9b9df5e729440c40a4c99fc
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2022-01-18 21:20:40 +0200

    add gsap and testing it out

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ menuToggle.addEventListener("click",function(){
     menuToggle.classList.toggle('active');
     sidebar.classList.toggle('active');
 });
+
+gsap.fromTo(".header", 
+            {duration: 1, opacity: 0, scale: 2},
+            {duration: 2,ease: "bounce.out", opacity: 1, scale: 1});
