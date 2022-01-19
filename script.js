55a7ef811f991152ecc8f4d4d8f1e873512d0121
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2022-01-19 13:43:17 +0200

    build project slider from scratch

--- script.js
+++ script.js
@@ -8,35 +8,35 @@ menuToggle.addEventListener("click", function () {
 
 const themeBtn = document.querySelector('.light-dark-button');
 
-function getCurrentTheme(){
-  let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  localStorage.getItem('canabrey.theme') ? theme = localStorage.getItem('canabrey.theme') : null;
-  return theme;
+function getCurrentTheme() {
+    let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    localStorage.getItem('canabrey.theme') ? theme = localStorage.getItem('canabrey.theme') : null;
+    return theme;
 }
 
-function loadTheme(theme){
-  const root = document.querySelector(':root');
-  if(theme === "light"){
-    themeBtn.innerHTML = "<i class='fas fa-moon'></i>"
-  } else {
-    themeBtn.innerHTML = "<i class='fas fa-sun'></i>"
-  }
-  root.setAttribute('color-scheme', `${theme}`);
+function loadTheme(theme) {
+    const root = document.querySelector(':root');
+    if (theme === "light") {
+        themeBtn.innerHTML = "<i class='fas fa-moon'></i>"
+    } else {
+        themeBtn.innerHTML = "<i class='fas fa-sun'></i>"
+    }
+    root.setAttribute('color-scheme', `${theme}`);
 }
 
 themeBtn.addEventListener('click', () => {
-  let theme = getCurrentTheme();
-  if(theme === 'dark'){
-    theme = 'light';
-  } else {
-    theme = 'dark';
-  }
-  localStorage.setItem('canabrey.theme', `${theme}`);
-  loadTheme(theme);
+    let theme = getCurrentTheme();
+    if (theme === 'dark') {
+        theme = 'light';
+    } else {
+        theme = 'dark';
+    }
+    localStorage.setItem('canabrey.theme', `${theme}`);
+    loadTheme(theme);
 })
 
 window.addEventListener('DOMContentLoaded', () => {
-  loadTheme(getCurrentTheme());
+    loadTheme(getCurrentTheme());
 })
 
 gsap.fromTo('.header',
@@ -46,3 +46,48 @@ gsap.fromTo('.header',
 gsap.fromTo('.sub-title',
     { duration: 6, opacity: 0, x: -500 },
     { duration: 2, delay: 5, ease: "power1.out", opacity: 1, x: 0 });
+
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+const appTitles = document.querySelectorAll('.app-title')
+
+let activeSlide = 0
+let activeTitle = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+    activeTitle++
+    
+    if (activeSlide > slides.length - 1 && activeTitle > appTitles.length - 1) {
+        activeSlide = 0
+        activeTitle = 0
+    }
+    setActiveSlide()
+    setActiveTitle()
+})
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+    activeTitle--
+
+    if (activeSlide < 0 && activeTitle < 0) {
+        activeSlide = slides.length - 1
+        activeTitle = appTitles.length - 1
+    }
+    setActiveSlide()
+    setActiveTitle()
+})
+
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+    })
+
+    slides[activeSlide].classList.add('active')
+}
+function setActiveTitle() {
+    appTitles.forEach(title => {
+        title.classList.remove('active')
+    })
+    appTitles[activeTitle].classList.add('active')
+}
