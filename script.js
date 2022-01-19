9be1dac1fc9b4a832f3515f7c2d163d8efb66ceb
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2022-01-19 11:23:11 +0200

    style about content

--- script.js
+++ script.js
@@ -6,6 +6,39 @@ menuToggle.addEventListener("click", function () {
     sidebar.classList.toggle('active');
 });
 
+const themeBtn = document.querySelector('.light-dark-button');
+
+function getCurrentTheme(){
+  let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  localStorage.getItem('canabrey.theme') ? theme = localStorage.getItem('canabrey.theme') : null;
+  return theme;
+}
+
+function loadTheme(theme){
+  const root = document.querySelector(':root');
+  if(theme === "light"){
+    themeBtn.innerHTML = "<i class='fas fa-moon'></i>"
+  } else {
+    themeBtn.innerHTML = "<i class='fas fa-sun'></i>"
+  }
+  root.setAttribute('color-scheme', `${theme}`);
+}
+
+themeBtn.addEventListener('click', () => {
+  let theme = getCurrentTheme();
+  if(theme === 'dark'){
+    theme = 'light';
+  } else {
+    theme = 'dark';
+  }
+  localStorage.setItem('canabrey.theme', `${theme}`);
+  loadTheme(theme);
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadTheme(getCurrentTheme());
+})
+
 gsap.fromTo('.header',
     { duration: 1, opacity: 0, scale: 5 },
     { duration: 2, ease: "power3.out", opacity: 1, scale: 1 });
@@ -13,11 +46,3 @@ gsap.fromTo('.header',
 gsap.fromTo('.sub-title',
     { duration: 6, opacity: 0, x: -500 },
     { duration: 2, delay: 5, ease: "power1.out", opacity: 1, x: 0 });
-
-TweenMax.set("#arrow", { xPercent: -50, yPercent: -50, transformOrigin: "center" });
-
-var motionPath = MorphSVGPlugin.pathDataToBezier('#line', { align: "#arrow" });
-
-var Intro = new TimelineMax({ repeat: 3, repeatDelay: 1 })
-    .from("#line", 4, { drawSVG: "0%", ease: Linear.easeNone })
-    .to('#arrow', 4, { bezier: { values: motionPath, type: "cubic", autoRotate: true }, ease: Linear.easeNone }, 0);
